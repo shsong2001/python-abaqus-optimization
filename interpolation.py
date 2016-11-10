8f16b06e1cd0553fe39fef3d8b18292f019eff60
JensPfeifle <jens.pfeifle@gmail.com>
2016-11-10 13:24:59 +0100

    Adjustment of point 3 to meet volume criterion

--- interpolation.py
+++ interpolation.py
@@ -61,32 +61,16 @@ def dumpData ( dataDict, filename ):
     file.close()
         
     
-def rotationVolume ( function ):
+def func_rotationVolume ( function ):
+    "Return volume from rotation around x-axis"
     result = integrate.quad(lambda x: function(x)*function(x),0,2)
     return math.pi*result[0]
-    
-def istEnformbar ( function, leftbound, rightbound ):
-    '''
-    Manufacturable only when there are no concave (f''<0) sections
-    or in this case (3rd order Polynomial), test for only one zero
-    Not well-implemented.
-    '''
-    function = np.poly1d(function)
-    criticalPoints = np.polyder(function).r # returns roots of the derivative
-    realCriticalPoints = criticalPoints[criticalPoints.imag==0].real # get real roots
-    secondDer = np.polyder(function,2) # calculate second derivative
-    testPoints = secondDer(realCriticalPoints) # get values at real roots
-    
-    crit = np.asarray(realCriticalPoints)
-    for i in range(len(crit)):
-        if testPoints[i] < 0 & leftbound > crit[i] > rightbound:
-            return False
-    return True
- 
-    criticalPoints = np.polyder(function).r # returns roots of the derivative
-    realCriticalPoints = criticalPoints[criticalPoints.imag==0].real # get real roots
-    secondDer = np.polyder(function,2) # calculate second derivative
-    return secondDer(realCriticalPoints) # get values at real roots
+
+def poly_rotationVolume (x,y):
+    "Generate interpolated polynomial from points and return volume from rotation around x-axis"
+    p = np.polyfit(x, y,2) # returns float64 array of coefficients from highest to lowest order
+    function = np.poly1d(p) # generate function from coefficients
+    return func_rotationVolume(function)
     
 
 #==============================================================================
@@ -98,13 +82,13 @@ def istEnformbar ( function, leftbound, rightbound ):
 # Read data from input.json
 with open('input.json','r') as f:
     input = json.load(f)
-    # Uncomment below to read from inputfile
-    input = {'p1': [0,15],'p2': [1,10],'p3': [2,17],'vol':14}
+    # Uncomment below to overwrite inputfile
+    #input = {'p1': [0,13],'p2': [1,10],'p3': [2,18],'vol':14}
     #
     
 keys = ('p1','p2','p3','vol')    
 points = [input[keys[i]] for i in range(len(keys)-1)]
-volume = input[keys[len(keys)-1]]
+target_volume = input[keys[len(keys)-1]]
 
 # split points into x and y
 points_x = [points[i][0] for i in range(len(points))]
@@ -113,32 +97,56 @@ points_y = [points[i][1] for i in range(len(points))]
 # Calculate interpolated polynomial
 p = np.polyfit(points_x, points_y,2) # returns float64 array of coefficients from highest to lowest order
 function = np.poly1d(p) # generate function from coefficients
-p = p.tolist() # convert p back to a list
+
+# Test Volume
+rotated_volume = func_rotationVolume(function)
+
+# loop to hit target volume by adjusting radius of point 3
+new_points_y = points_y[:] # duplicate the list of points
+new_vol = rotated_volume
+
+if rotated_volume > target_volume:
+    while (new_vol > target_volume):
+        new_points_y[2] -= 1
+        new_vol = poly_rotationVolume(points_x,new_points_y)
+else: 
+    while (new_vol < target_volume):
+        new_points_y[2] += 1
+        new_vol = poly_rotationVolume(points_x,new_points_y)
+        
+volume_error = new_vol - target_volume
+
+# Recalculate interpolated polynomial with new points
+p = np.polyfit(points_x, new_points_y,2) # returns float64 array of coefficients from highest to lowest order
+new_function = np.poly1d(p) # generate function from coefficients
+     
+        
+# Write data to json 
+p = p.tolist() # convert p back to a list for dump       
+coeffnames = ('a','b','c') # must be same length as p
+coeffs = {coeffnames[i]: p[i] for i in range(0,len(p))}
+dumpData(coeffs, 'output.json')  
+
+#==============================================================================
+# 
+#Plotting
+#==============================================================================
 
 # Generate function values for plotting
 function_x = np.linspace(points_x[0],points_x[-1],50)
 function_y = function(function_x)
-
-# Tests
-rotatedVolume = rotationVolume(function)
-#functionMin = minV(function)
-#functionMax = maxV(function)
-#entformbar = istEnformbar(function, 0, 3)
-
-coeffnames = ('a','b','c') # must be same length as p
-coeffs = {coeffnames[i]: p[i] for i in range(0,len(p))}
-dumpData(coeffs, 'int_output.json')  
-#dumpData(inputdata, 'input.json')
+new_function_y = new_function(function_x)
 
 # Plots
-plt.axis([-1, 3, -20, 20])
+plt.axis([-1, 3, -20, 30])
 plt.title("Points and Polynomial Fit")
-plt.ylabel("Abstand von unten")
-plt.xlabel("Radius")
+plt.ylabel("Radius")
 plt.hlines(0,0,2,'k','-.')
-plt.plot(points_x,points_y,'ro') # add points to plot
-plt.plot(function_x,function_y,'g') # add polynomial to plot
-plt.plot(function_x,-function_y,'g') # add polynomial to plot
+plt.plot(points_x,points_y,'r+') # add points to plot
+plt.plot(points_x,new_points_y,'ro') # add new points to plot
+plt.plot(function_x,function_y,'b--') # add polynomial to plot
+plt.plot(function_x,new_function_y,'g') # add new polynomial to plot
+plt.plot(function_x,-new_function_y,'g') # add negativ side
 #savePlotPDF('points_polyfit.pdf') 
 plt.show()
 
